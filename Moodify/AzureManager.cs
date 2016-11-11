9aae661748b8fa2e2868a058f35e8bf819706cf8
Jason Goodsell <jasongoodsell@studentpartner.com>
2016-11-11 18:48:06 +1300

    Azure manager class added.

---
+++ Moodify/AzureManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moodify
+{
+    public partial class AzureManager
+    {
+
+        static AzureManager defaultInstance = new AzureManager();
+        MobileServiceClient client;
+
+        private AzureManager()
+        {
+            this.client = new MobileServiceClient(App.ApplicationURL);
+        }
+
+        public MobileServiceClient CurrentClient
+        {
+            get { return client; }
+        }
+    }
+}
